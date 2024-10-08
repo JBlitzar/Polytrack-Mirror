e893266a46e30c539609ef6b05ee5f84232892e3
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2024-10-08 13:03:08 -0700

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,1 +1,3 @@
-mv "$(find . -mindepth 1 -type d -print -quit)" "$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 13)"
+mv "$(find . -mindepth 1 -type d -print -quit)" "$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 13)";
+git commit -m "moved";
+git push origin main;
